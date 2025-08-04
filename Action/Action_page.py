5c144f58f5cb8058e88a76221aa596637284d0ca
chiomzz <chiomaikerionwu@gmail.com>
2025-08-04 16:21:44 -0600

    code updates

--- Action/Action_page.py
+++ Action/Action_page.py
@@ -1,3 +1,8 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from Locators.Locator_page import LocatorPage
+
+
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
@@ -5,3 +10,17 @@ class LoginPage:
     def open_login_page(self, url):
         self.driver.get(url)
 
+    def enter_username(self, username):
+        username_field = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(LocatorPage.USERNAME))
+        username_field.send_keys(username)
+
+    def enter_password(self, password):
+        password_field = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(LocatorPage.PASSWORD))
+        password_field.send_keys(password)
+
+    def click_login(self):
+        login_field = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located(LocatorPage.LOGIN))
+        login_field.click()
--- Locators/Locator_page.py
+++ Locators/Locator_page.py
@@ -5,3 +5,23 @@ class LocatorPage:
     USERNAME = (By.XPATH, '//*[@id="user-name"]')
     PASSWORD = (By.XPATH, '//*[@id="password"]')
     LOGIN = (By.XPATH, '//*[@id="login-button"]')
+
+
+    ADD_TO_CART_BACKPACK = (By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')
+    ADD_TO_CART_BOLT_TSHIRT = (By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]')
+    ADD_TO_CART_ONESIE = (By.XPATH, '//*[@id="add-to-cart-sauce-labs-onesie"]')
+    ADD_TO_CART_BIKE_LIGHT = (By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]')
+    ADD_TO_CART_FLEECE_JACKET = (By.XPATH, '//*[@id="add-to-cart-sauce-labs-fleece-jacket"]')
+    ADD_TO_CART_TSHIRT_RED = (By.XPATH, '//*[@id="add-to-cart-test.allthethings()-t-shirt-(red)"]')
+
+    SHOPPING_CART = (By.XPATH,'//*[@id="shopping_cart_container"]/a')
+    CHECKOUT = (By.XPATH, '//*[@id="checkout"]')
+    FIRSTNAME = (By.XPATH, '//*[@id="first-name"]')
+    LASTNAME = (By.XPATH, '//*[@id="last-name"]')
+    ZIPCODE = (By.XPATH, '//*[@id="postal-code"]')
+    CONTINUE = (By.XPATH, '//*[@id="continue"]')
+    FINISH = (By.XPATH, '//*[@id="finish"]')
+
+    BURGER_MENU = (By.XPATH, '//*[@id="react-burger-menu-btn"]')
+    LOGOUT = (By.XPATH, '//*[@id="logout_sidebar_link"]')
+
